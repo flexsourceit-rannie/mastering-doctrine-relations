072479cc90d0617329618d746839f4e4dde51ecd
Rannie Ollit <rannie.ollit@eonx.com>
2022-02-02 07:19:18 +0800

    initial commit

---
+++ src/Controller/AnswerController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Answer;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AnswerController extends AbstractController
+{
+    /**
+     * @Route("/answers/{id}/vote", methods="POST", name="answer_vote")
+     */
+    public function answerVote(Answer $answer, EntityManagerInterface $manager, LoggerInterface $logger, Request $request, $id): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $direction = $data['direction'] ?? 'up';
+
+
+
+        // use real logic here to save this to the database
+        if ($direction === 'up') {
+            $logger->info('Voting up!');
+            $answer->setVotes($answer->getVotes() + 1);
+        } else {
+            $logger->info('Voting down!');
+            $answer->setVotes($answer->getVotes() - 1);
+        }
+
+        $manager->flush();
+
+        return $this->json(['votes' => $answer->getVotes()]);
+    }
+}
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Answer;
+use App\Entity\Question;
+use App\Factory\AnswerFactory;
+use App\Factory\QuestionFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $questions = QuestionFactory::createMany(20);
+
+        QuestionFactory::new()
+            ->unpublished()
+            ->many(5)
+            ->create()
+        ;
+
+        AnswerFactory::createMany(100, function () use ($questions) {
+            return [
+                'question' => $questions[array_rand($questions)]
+            ];
+        });
+
+        $manager->flush();
+    }
+}
